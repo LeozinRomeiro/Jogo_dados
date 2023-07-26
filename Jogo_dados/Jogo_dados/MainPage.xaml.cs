0524bbde4ca46bce745c4d1f849729ac84babd9f
Aluno <Aluno@SNCTM202LABF02>
2023-07-25 22:22:10 -0300

    Adicionar arquivos de projeto.

---
+++ Jogo_dados/Jogo_dados/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Jogo_dados
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void botao_play_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page_dado());
+        }
+    }
+}
---
+++ Jogo_dados/Jogo_dados/Page_dado.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Jogo_dados
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page_dado : ContentPage
+    {
+        public Page_dado()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            int sorteado = random.Next(1, 6);
+            //imagem_dado.Source = "dado" + sorteado;
+
+            switch(sorteado)
+            {
+                case 1:
+                    imagem_dado.Source = "dado1";
+                    break;
+                case 2:
+                    imagem_dado.Source = "dado2";
+                    break;
+                case 3:
+                    imagem_dado.Source = "dado3";
+                    break;
+                case 4:
+                    imagem_dado.Source = "dado4";
+                    break;
+                case 5:
+                    imagem_dado.Source = "dado5";
+                    break;
+                case 6:
+                    imagem_dado.Source = "dado6";
+                    break;
+            }
+
+        }
+
+        private async void imagem_voltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
